b85ac48e0591a1af7adca38e55b4710a849e6924
sujuque <97406386+sujuque@users.noreply.github.com>
2024-12-28 15:24:17 +0100

    Add dist folder and update config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default {
-  appType: "mpa", // Multi-page application
+  appType: "mpa", 
   plugins: [ViteMinifyPlugin()],
   root: "./src", // This is where your HTML files inside the src folder are located
   build: {
     outDir: "../dist", // Output folder for built files
     emptyOutDir: true, // Clean the output directory before building
-    assetsInclude: ['**/*.woff2', '**/*.woff', '**/*.ttf', '**/*.eot'], // Include font files
     rollupOptions: {
       input: {
-        index: "./index.html", // index.html is outside the src folder
-        about: "./src/about.html", // about.html inside src
-        animation: "./src/animation.html", // animation.html inside src
-        illustration: "./src/illustration.html", // illustration.html inside src
-        projects: "./src/projects.html", // projects.html inside src
-        deta_surf: "./src/projects/deta_surf.html", // deta_surf.html inside the src/projects folder
-        gibbon: "./src/projects/gibbon.html", // gibbon.html inside the src/projects folder
+        index: "./src/index.html", 
+        about: "./src/about.html", 
+        animation: "./src/animation.html", 
+        illustration: "./src/illustration.html", 
+        projects: "./src/projects.html", 
+        deta_surf: "./src/projects/deta_surf.html", 
+        gibbon: "./src/projects/gibbon.html", 
       },
     },
   },
 };
+
 
